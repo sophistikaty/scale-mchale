be0f1ed5fb45c99f8c3ac88ca65a136c80b4aadb
Kaitlynn Hendricks <khendricks@researchinnovations.com>
2019-01-18 14:43:09 -0500

    blank ng2

--- src/ts/measureConvert.ts
+++
@@ -1,7 +0,0 @@
-// products.constant('measureConvert', {
-// 	drops: { base:1, tsp:0.01, tbs:0.0033, cups:0.0002 },
-// 	tsp:{ base:1, drops:100, tbs:0.3333, cups:0.0208 },
-// 	tbs:{ base:1, drops:300, tsp:3, cups:0.0625 },
-// 	cups:{ base:1, drops:4800, tsp:48, tbs:16 }
-	
-// })
